39582a23f1ef7e3d202175c1458edbe54a143f1a
Laercio Lima <llaercio.lima@gmail.com>
2018-06-04 18:23:46 -0300

    criacao de tela de perfil de usuario e bloqueio de modulo

--- views/usuario/home.php
+++ views/usuario/home.php
@@ -1,183 +1,195 @@
 <?php
 
-if ($usuario_logado['perfil'] != 'admin') {?>
 
 
-    <h1 class="text-center">Módulos</h1>
-    <div class="row">
+
+if ($usuario_logado['perfil'] != 'admin') {
+    
+    $modulo_liberado = 1;
+    ?>
 
 
-        <?php foreach ($modulos as $modulo) {?>
+<h1 class="text-center">Módulos</h1>
+<div class="row">
 
-        <div class="col-sm-6">
 
-            <div class="panel panel-success">
-                <div class="panel-heading">
-                    <?php echo $modulo->getNome(); ?>
-                </div>
-                <div class="panel-body">
-                    <?php echo $modulo->getDescricao(); ?>
-                </div>
-                <div class="panel-footer">
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
-                            style="width:<?php echo $modulo->getQntdSinaisAprendidos() * 100 / $modulo->getQntdSinais(); ?>%">
-                            <?php echo $modulo->getQntdSinaisAprendidos() . "/" . $modulo->getQntdSinais(); ?>
-                        </div>
+    <?php foreach ($modulos as $modulo) {?>
+
+    <div class="col-sm-6">
+
+        <div class="panel panel-<?php $modulo_liberado >= 0.7 ? 'success' : 'default'?>">
+            <div class="panel-heading">
+                <?php echo $modulo->getNome(); ?>
+            </div>
+            <div class="panel-body">
+                <?php echo $modulo->getDescricao(); ?>
+            </div>
+            <div class="panel-footer">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"
+                        style="width:<?php echo $modulo->getQntdSinaisAprendidos() * 100 / $modulo->getQntdSinais(); ?>%">
+                        <?php echo $modulo->getQntdSinaisAprendidos() . "/" . $modulo->getQntdSinais(); ?>
                     </div>
-                    <div class="row">
-                        <button type="button" class="btn btn-default col-sm-7" onclick="location.href = '?controller=categoria&action=lista&modulo=<?php echo $modulo->getId(); ?>'">
-                            <span class="glyphicon glyphicon-play"></span> Praticar</button>
-
-
-                        <button type="button" class="btn btn-default  col-sm-4 col-md-offset-1" onclick="location.href = '?controller=atividade&action=lista&modulo=<?php echo $modulo->getId(); ?>'">
-                            <span class="glyphicon glyphicon-list"></span> Atividades</button>
-                    </div>
-
-
                 </div>
+                <div class="row">
+                    <?php if($modulo_liberado >= 0.7) {?>
+                    <button type="button" class="btn btn-default col-sm-7" onclick="location.href = '?controller=categoria&action=lista&modulo=<?php echo $modulo->getId(); ?>'">
+                        <span class="glyphicon glyphicon-play"></span> Praticar</button>
+
+
+                    <button type="button" class="btn btn-default  col-sm-4 col-md-offset-1" onclick="location.href = '?controller=atividade&action=lista&modulo=<?php echo $modulo->getId(); ?>'">
+                        <span class="glyphicon glyphicon-list"></span> Atividades</button>
+
+
+                    <?php } ?>
+                </div>
+
+
             </div>
         </div>
-
-        <?php
-}?>
-
-
     </div>
 
     <?php
 
+$modulo_liberado = $modulo->getQntdSinaisAprendidos() / $modulo->getQntdSinais();
+}?>
+
+
+</div>
+
+<?php
+
 } else {?>
 
-        <h1 class="text-center">Painel de controle</h1>
+<h1 class="text-center">Painel de controle</h1>
 
-            <div class="col-sm-4">
-
-
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Módulo</div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=modulo&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
-
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=modulo&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
+<div class="col-sm-4">
 
 
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Módulo</div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=modulo&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
 
-            <div class="col-sm-4">
-
-
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Categorias</div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=categoria&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
-
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=categoria&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=modulo&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
 
 
 
-            <div class="col-sm-4">
+<div class="col-sm-4">
 
 
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Sinais </div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=sinal&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Categorias</div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=categoria&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
 
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=sinal&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-
-
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Atividades</div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=atividade&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
-
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=atividade&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-
-
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Movimentos </div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=movimento&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
-
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=movimento&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-4">
-
-
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Configurações de mão</div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=configuracaoDeMao&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
-
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=configuracaoDeMao&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=categoria&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
 
 
 
-             <div class="col-sm-4">
+<div class="col-sm-4">
 
 
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Expressões faciais</div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=expressaoFacial&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Sinais </div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=sinal&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
 
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=expressaoFacial&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=sinal&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
 
-
-
-            <div class="col-sm-4">
+<div class="col-sm-4">
 
 
-                <div class="panel panel-info">
-                    <div class="panel-heading">Gerenciar Pontos de articulação</div>
-                    <div class="panel-body">
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=pontoDeArticulacao&action=add'">
-                            <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Atividades</div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=atividade&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
 
-                        <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=pontoDeArticulacao&action=index'">
-                            <span class="glyphicon glyphicon-th-list"></span> Listar</button>
-                    </div>
-                </div>
-            </div>
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=atividade&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
+
+<div class="col-sm-4">
 
 
-        <?php } ?>
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Movimentos </div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=movimento&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=movimento&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
+
+<div class="col-sm-4">
+
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Configurações de mão</div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=configuracaoDeMao&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=configuracaoDeMao&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="col-sm-4">
+
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Expressões faciais</div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=expressaoFacial&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=expressaoFacial&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="col-sm-4">
+
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Gerenciar Pontos de articulação</div>
+        <div class="panel-body">
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=pontoDeArticulacao&action=add'">
+                <span class="glyphicon glyphicon-plus-sign"></span> Novo</button>
+
+            <button type="button" class="btn btn-default btn-block" onclick="location.href = '?controller=pontoDeArticulacao&action=index'">
+                <span class="glyphicon glyphicon-th-list"></span> Listar</button>
+        </div>
+    </div>
+</div>
+
+
+<?php } ?>
---
+++ views/usuario/perfil.php
@@ -0,0 +1,36 @@
+https://bootsnipp.com/snippets/featured/profile-box
+
+
+
+
+<div class="container" style="margin-top: 20px; margin-bottom: 20px;">
+    <div class="row panel">
+        <div class="col-md-4 bg_blur ">
+            <a href="#" class="follow_btn hidden-xs">Solicitar amizade</a>
+        </div>
+        <div class="col-md-8  col-xs-12">
+            <img src="http://lorempixel.com/output/people-q-c-100-100-1.jpg" class="img-thumbnail picture hidden-xs" />
+            <img src="http://lorempixel.com/output/people-q-c-100-100-1.jpg" class="img-thumbnail visible-xs picture_mob" />
+            <div class="header">
+                <h1>Laercio Lima</h1>
+                <h4>@laercio</h4>
+                <span>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." "There is
+                    no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="row nav">
+        <div class="col-md-4"></div>
+        <div class="col-md-8 col-xs-12" style="margin: 0px;padding: 0px;">
+            <div class="col-md-4 col-xs-4 well">
+                <i class="fa fa-weixin fa-lg"></i> 16</div>
+            <div class="col-md-4 col-xs-4 well">
+                <i class="fa fa-heart-o fa-lg"></i> 14</div>
+            <div class="col-md-4 col-xs-4 well">
+                <i class="fa fa-thumbs-o-up fa-lg"></i> 26</div>
+        </div>
+    </div>
+
+
+</div>
